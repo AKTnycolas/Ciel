68e6bcea5452d9dbfa7c0d6b5a527303d55813f2
Haruka Yamamoto <87913906+HarukaYamamoto0@users.noreply.github.com>
2022-03-16 16:35:08 -0300

    update index.js

--- src/client/vulkava/index.js
+++ src/client/vulkava/index.js
@@ -1,22 +1,7 @@
-const { Vulkava } = require("vulkava");
-
-module.exports = {
-  async start(client) {
-    // this is a public lavalink, I used it for demo
-    client.vulkava = new Vulkava({
-      nodes: [
-        {
-          id: "Node 1",
-          hostname: "lavalink.oops.wtf",
-          port: 2000,
-          password: "www.freelavalink.ga",
-          secure: false,
-        },
-      ],
-      sendWS: (guildId, payload) => {
-        const guild = client.guilds.cache.get(guildId);
-        if (guild) guild.shard.send(payload);
-      },
-    });
-  },
-};
+class oi {
+  construtor() {
+    this.data = {
+      0
+    }
+  }
+}
