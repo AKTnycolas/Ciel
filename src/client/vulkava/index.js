a873f3f89827bde703dc15068571cbcf54e35ee2
Haruka Yamamoto <87913906+HarukaYamamoto0@users.noreply.github.com>
2022-03-16 16:46:36 -0300

    update index.js

--- src/client/vulkava/index.js
+++ src/client/vulkava/index.js
@@ -1,13 +1,20 @@
-class oi {
-  construtor() {
-    this.data = {
-      0: 0
-    }
-  }
-  
-  get oii() {
-    return this.data
+const { Client } = require("discord.js");
+require("dotenv").config();
+
+async function start() {
+  try {
+    const client = new Client({ intents: 70655 });
+    global.fetch = require("node-fetch");
+
+    await require("./database/index").start(client);
+    await require("./client/vulkava/index").start(client);
+    await require("./client/fileLoader").load(client);
+
+    await client.login(process.env.tokenBot);
+    console.log("[INDEX] - index carregada com sucesso");
+  } catch (err) {
+    console.log(err);
   }
 }
 
-console.log(new oi());
+start();
